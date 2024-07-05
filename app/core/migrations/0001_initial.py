8218affe94abb22956e6c7f66af68b1637c056b3
DominikZurawski <38438473+DominikZurawski@users.noreply.github.com>
2024-07-05 10:31:22 +0200

    Build user API

--- app/core/migrations/0001_initial.py
+++ app/core/migrations/0001_initial.py
@@ -32,8 +32,8 @@ class Migration(migrations.Migration):
                 ('groups', models.ManyToManyField(blank=True,
                  help_text='The groups this user belongs to. A user will get all permissions \
                      granted to each of their groups.',
-                 related_name='user_set', related_query_name='user', to='auth.Group',
-                 verbose_name='groups')),
+                 related_name='user_set', related_query_name='user',
+                 to='auth.Group', verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(blank=True,
                  help_text='Specific permissions for this user.',
                  related_name='user_set', related_query_name='user',
